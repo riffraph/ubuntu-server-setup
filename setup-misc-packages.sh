0df6a3e169c74e521f91cde7fea00c0284fd7388
Raphael Chan <raphaelc@gmail.com>
2022-08-08 14:11:54 +0200

    update apt list after updating with docker https repositories

--- setup-misc-packages.sh
+++ setup-misc-packages.sh
@@ -2,7 +2,7 @@
 
 function setupZsh() {
     apt install zsh
-    
+
     # from https://ohmyz.sh/#install
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
@@ -20,5 +20,6 @@ function installDocker() {
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     # install docker engine
-    apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    apt update
+    yes Y | apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 }
