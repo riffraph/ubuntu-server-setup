f3d9aa583ba6efa9bccb5c9d9102a5f1083d45da
Raphael Chan <raphaelc@gmail.com>
2022-08-23 11:02:54 +0200

    added script to disable IPV6 using the Grub method

--- _setup-network.sh
+++ _setup-network.sh
@@ -4,6 +4,7 @@
 function disableIPv6()
 {
     if grep -P 0 /sys/module/ipv6/parameters/disable; then
+        # Update sysctl method ... may not work for later versions on Ubuntu
         if grep 'net.ipv6.conf.all.disable_ipv6' /etc/sysctl.conf
         then
             sed -re 's/^(\#?)(net.ipv6.conf.all.disable_ipv6=)(.*)/net.ipv6.conf.all.disable_ipv6=1/' -i /etc/sysctl.conf
@@ -25,8 +26,15 @@ function disableIPv6()
             echo "net.ipv6.conf.lo.disable_ipv6=1" >> /etc/sysctl.conf
         fi
         
-
         sysctl -p
+
+
+        # Update GRUB method
+
+        sed -re 's/^(\#?)(GRUB_CMDLINE_LINUX_DEFAULT=)(.*)/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash ipv6.disable=1"/' -i /etc/default/grub
+        sed -re 's/^(\#?)(GRUB_CMDLINE_LINUX=)(.*)/GRUB_CMDLINE_LINUX="ipv6.disable=1"/' -i /etc/default/grub
+
+        update-grub
     fi
 }
 
