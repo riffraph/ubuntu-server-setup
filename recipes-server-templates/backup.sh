642078dcb20233aa84aabe07c218e7b710977d4b
Raphael Chan <raphaelc@gmail.com>
2022-09-30 16:00:37 +0200

    changed to use pg_dumpall within the docker container

--- recipes-server-templates/backup.sh
+++ recipes-server-templates/backup.sh
@@ -15,14 +15,12 @@ echo "$(date "+%d.%m.%Y %T") INFO: ${0} started."
 
 mkdir -p ${BACKUP_FOLDER}
 
-postgresHost=$(grep "POSTGRES_HOST=" ${SCRIPT_FOLDER}/${ENVIRONMENT_FILE} | sed 's/.*=//')
-postgresPort=$(grep "POSTGRES_PORT=" ${SCRIPT_FOLDER}/${ENVIRONMENT_FILE} | sed 's/.*=//')
-postgresDB=$(grep "POSTGRES_DB=" ${SCRIPT_FOLDER}/${ENVIRONMENT_FILE} | sed 's/.*=//')
 postgresUser=$(grep "POSTGRES_USER=" ${SCRIPT_FOLDER}/${ENVIRONMENT_FILE} | sed 's/.*=//')
-postgresPassword=$(grep "POSTGRES_PASSWORD=" ${SCRIPT_FOLDER}/${ENVIRONMENT_FILE} | sed 's/.*=//')
 
 echo "$(date "+%d.%m.%Y %T") INFO: Backing up database to ${BACKUP_FOLDER}."
-PGPASSWORD=${postgresPassword} pg_dump --inserts --column-inserts --username=${postgresUser}--host=${postgresHost} --port=${postgresPort} ${postgresDB} > ${BACKUP_FOLDER}/dbbackup.sql
+docker exec -it recipes-db pg_dumpall -U ${postgresUser} -f /var/lib/postgresql/data/dbbackup.sql
+
+mv postgresql/dbbackup.sql ${BACKUP_FOLDER}/
 
 
 echo "$(date "+%d.%m.%Y %T") INFO: ${0} completed."
